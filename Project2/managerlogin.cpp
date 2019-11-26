b86edf2708bd9244a3340ebd525c578d0511d71c
tdragon00 <47230462+tdragon00@users.noreply.github.com>
2019-11-26 11:13:36 -0800

    testing the login

--- Project2/managerlogin.cpp
+++ Project2/managerlogin.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include "managerlogin.h"
 #include "adminlogin.h"
 #include "mainwindow.h"
@@ -33,3 +34,59 @@ void managerLogin::on_backLogin_clicked()
 }
 
 
+=======
+#include "managerlogin.h"
+#include "adminlogin.h"
+#include "mainwindow.h"
+#include "ui_managerlogin.h"
+#include <QUrl>
+#include <QDesktopServices>
+#include <QMessageBox>
+#include "manager_member_database.h"
+#include "manager_item_database.h"
+
+managerLogin::managerLogin( QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::managerLogin)
+{
+    ui->setupUi(this);
+    if (!conn.connOpen())
+        ui->statusLine->setText("Failed to open the database");
+    else
+        ui->statusLine->setText("Database Connected...");
+
+//    ui->lineEdit->setText(username);
+}
+
+managerLogin::~managerLogin()
+{
+    delete ui;
+}
+
+void managerLogin::on_backLogin_clicked()
+{
+    hide();
+    MainWindow *mainWindow = new MainWindow();
+    mainWindow->show();
+}
+
+
+
+
+
+void managerLogin::on_MemberData_clicked()
+{
+
+    hide();
+    manager_member_database *Manager_Member_Database =new manager_member_database();
+    Manager_Member_Database->show();
+}
+
+void managerLogin::on_Item_data_clicked()
+{
+
+    hide();
+    manager_item_database *Manager_Item_Database =new manager_item_database();
+    Manager_Item_Database->show();
+}
+>>>>>>> Stashed changes
--- Project2/managerlogin.h
+++ Project2/managerlogin.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef MANAGERLOGIN_H
 #define MANAGERLOGIN_H
 
@@ -30,3 +31,41 @@ private:
 };
 
 #endif // managerLogin_H
+=======
+#ifndef MANAGERLOGIN_H
+#define MANAGERLOGIN_H
+
+#include <QDialog>
+#include "mainwindow.h"
+
+namespace Ui {
+class managerLogin;
+}
+
+class managerLogin : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit managerLogin( QWidget *parent = nullptr);
+    ~managerLogin();
+
+public:
+    MainWindow conn;
+private slots:
+    void on_backLogin_clicked();
+
+
+
+    void on_pushButton_clicked();
+
+    void on_MemberData_clicked();
+
+    void on_Item_data_clicked();
+
+private:
+    Ui::managerLogin *ui;
+};
+
+#endif // managerLogin_H
+>>>>>>> Stashed changes
