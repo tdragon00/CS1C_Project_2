d31d325f87dfdb3b9e3814074aaffc3aa847c62a
tdragon00 <47230462+tdragon00@users.noreply.github.com>
2019-11-25 17:24:03 -0800

    added a table to view

--- Project2/admin_member_database.cpp
+++ Project2/admin_member_database.cpp
@@ -1,6 +1,17 @@
 #include "admin_member_database.h"
 #include "ui_admin_member_database.h"
 #include "adminlogin.h"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "admincreate.h"
+#include "adminlogin.h"
+#include "managerlogin.h"
+#include "accountconfirm.h"
+#include "createcheck.h"
+#include <QMessageBox>
+#include <QPixmap>
+#include <QDesktopServices>
+#include <QUrl>
 
 
 Admin_Member_Database::Admin_Member_Database(QWidget *parent) :
@@ -20,4 +31,21 @@ void Admin_Member_Database::on_pushButton_clicked()
     hide();
     adminLogin *administratorLogin = new adminLogin;
     administratorLogin -> show();
+}
+
+void Admin_Member_Database::on_pushButton_2_clicked()
+{
+    MainWindow conn;
+    QSqlQueryModel * modal = new QSqlQueryModel();
+
+    conn.connOpen();
+    QSqlQuery* qry = new QSqlQuery(conn.mydb);
+
+    qry->prepare("Select name,memberNum, status, expDate from customers");
+
+    qry->exec();
+    modal->setQuery(*qry);
+    ui->tableView->setModel(modal);
+
+    conn.connClose();
 }
--- Project2/manager_member_database.cpp
+++ Project2/manager_member_database.cpp
@@ -1,6 +1,14 @@
 #include "manager_member_database.h"
 #include "ui_manager_member_database.h"
-
+#include "admincreate.h"
+#include "adminlogin.h"
+#include "managerlogin.h"
+#include "accountconfirm.h"
+#include "createcheck.h"
+#include <QMessageBox>
+#include <QPixmap>
+#include <QDesktopServices>
+#include <QUrl>
 manager_member_database::manager_member_database(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::manager_member_database)
