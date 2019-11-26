6e92540d3b454ecee89659416108cac1a7e0a220
Aziiz0 <54380763+Aziiz0@users.noreply.github.com>
2019-11-26 11:12:17 -0800

    Uploading txt to db

--- Project2/adminlogin.cpp
+++ Project2/adminlogin.cpp
@@ -53,37 +53,271 @@ admin_member_database->show();
 
 void adminLogin::on_pushButton_clicked()
 {
-    qDebug() << "Extracting Customers from .txt to .db";
-    QString name;
-    QString memberNum;
-    QString status;
-    QString expDate;
-
-    QFile file(":/textfiles/Resource Stuff/Database textfiles/warehouse shoppers.txt");
-    if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
-    {
-        qDebug() << "Could not Read File";
-    }
-    QTextStream in(&file);
-
     if (!conn.connOpen())
     {
         qDebug() << "Failed To Open the Database";
     }
     conn.connOpen();
-    QSqlQuery qry;
+    QSqlQuery db;
 
-    while(!in.atEnd())
-    {
-        name = in.readLine();
-        memberNum = in.readLine();
-        status = in.readLine();
-        expDate = in.readLine();
+    {//Refreshing Customer db
+        qDebug() << "Begining extraction for cusomters from .txt to .db";
+        QString name;
+        QString memberNum;
+        QString status;
+        QString expDate;
 
-        qDebug() << "Inserting Customer " << name;
+        QFile file(":/textfiles/Resource Stuff/Database textfiles/warehouse shoppers.txt");
+        if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
+        {
+            qDebug() << "Could not Read File";
+        }
+        QTextStream in(&file);
 
-        qry.exec("insert into customers (name, memberNum, status, expDate)"
+        qDebug() << "Deleting cusomter db";
+        db.exec("delete from customers");
+
+        while(!in.atEnd())
+        {
+            name = in.readLine();
+            memberNum = in.readLine();
+            status = in.readLine();
+            expDate = in.readLine();
+
+            qDebug() << "Inserting Customer " << name;
+
+            db.exec("insert into customers (name, memberNum, status, expDate)"
                     "values ('"+name+"','"+memberNum+"','"+status+"','"+expDate+"')");
+            if(!db.exec())
+            {
+                qDebug() << "ERROR";
+            }
+        }
+    }
+    {//Refreshing Day 1 Sales Report db
+        qDebug() << "Begining extraction for Day 1 sales report from .txt to .db";
+        QString purchaseDate;
+        QString id;
+        QString productName;
+        QString price;
+        QString purchaseQty;
+        QString status;
+
+        QFile file(":/textfiles/Resource Stuff/Database textfiles/day1.txt");
+        if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
+        {
+            qDebug() << "Could not Read File";
+        }
+        QTextStream in(&file);
+
+        qDebug() << "Deleting salesReport db";
+        db.exec("delete from salesReport");
+
+        while(!in.atEnd())
+        {
+            purchaseDate = in.readLine();
+            id = in.readLine();
+            productName = in.readLine();
+            price = in.readLine();
+            purchaseQty = in.readLine();
+            status = in.readLine();
+
+            qDebug() << "Inserting purchase of " << productName << " for customer " << id;
+
+            db.exec("insert into salesReport (purchaseDate, id, productName, price, purchaseQty, status, day)"
+                    "values ('"+purchaseDate+"','"+id+"','"+productName+"','"+price+"','"+purchaseQty+"','"+status+"','"+"1"+"')");
+            if(!db.exec())
+            {
+                qDebug() << "ERROR";
+            }
+        }
+    }
+    {//Refreshing Day 2 Sales Report db
+        qDebug() << "Begining extraction for Day 2 sales report from .txt to .db";
+        QString purchaseDate;
+        QString id;
+        QString productName;
+        QString price;
+        QString purchaseQty;
+        QString status;
+
+        QFile file(":/textfiles/Resource Stuff/Database textfiles/day2.txt");
+        if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
+        {
+            qDebug() << "Could not Read File";
+        }
+        QTextStream in(&file);
+
+        while(!in.atEnd())
+        {
+            purchaseDate = in.readLine();
+            id = in.readLine();
+            productName = in.readLine();
+            price = in.readLine();
+            purchaseQty = in.readLine();
+            status = in.readLine();
+
+            qDebug() << "Inserting purchase of " << productName << " for customer " << id;
+
+            db.exec("insert into salesReport (purchaseDate, id, productName, price, purchaseQty, status, day)"
+                    "values ('"+purchaseDate+"','"+id+"','"+productName+"','"+price+"','"+purchaseQty+"','"+status+"','"+"2"+"')");
+        }
+    }
+    {//Refreshing Day 3 Sales Report db
+        qDebug() << "Begining extraction for Day 3 sales report from .txt to .db";
+        QString purchaseDate;
+        QString id;
+        QString productName;
+        QString price;
+        QString purchaseQty;
+        QString status;
+
+        QFile file(":/textfiles/Resource Stuff/Database textfiles/day3.txt");
+        if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
+        {
+            qDebug() << "Could not Read File";
+        }
+        QTextStream in(&file);
+
+        while(!in.atEnd())
+        {
+            purchaseDate = in.readLine();
+            id = in.readLine();
+            productName = in.readLine();
+            price = in.readLine();
+            purchaseQty = in.readLine();
+            status = in.readLine();
+
+            qDebug() << "Inserting purchase of " << productName << " for customer " << id;
+
+            db.exec("insert into salesReport (purchaseDate, id, productName, price, purchaseQty, status, day)"
+                    "values ('"+purchaseDate+"','"+id+"','"+productName+"','"+price+"','"+purchaseQty+"','"+status+"','"+"3"+"')");
+        }
+    }
+    {//Refreshing Day 4 Sales Report db
+        qDebug() << "Begining extraction for Day 4 sales report from .txt to .db";
+        QString purchaseDate;
+        QString id;
+        QString productName;
+        QString price;
+        QString purchaseQty;
+        QString status;
+
+        QFile file(":/textfiles/Resource Stuff/Database textfiles/day4.txt");
+        if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
+        {
+            qDebug() << "Could not Read File";
+        }
+        QTextStream in(&file);
+
+        while(!in.atEnd())
+        {
+            purchaseDate = in.readLine();
+            id = in.readLine();
+            productName = in.readLine();
+            price = in.readLine();
+            purchaseQty = in.readLine();
+            status = in.readLine();
+
+            qDebug() << "Inserting purchase of " << productName << " for customer " << id;
+
+            db.exec("insert into salesReport (purchaseDate, id, productName, price, purchaseQty, status, day)"
+                    "values ('"+purchaseDate+"','"+id+"','"+productName+"','"+price+"','"+purchaseQty+"','"+status+"','"+"4"+"')");
+        }
+    }
+    {//Refreshing Day 5 Sales Report db
+        qDebug() << "Begining extraction for Day 5 sales report from .txt to .db";
+        QString purchaseDate;
+        QString id;
+        QString productName;
+        QString price;
+        QString purchaseQty;
+        QString status;
+
+        QFile file(":/textfiles/Resource Stuff/Database textfiles/day5.txt");
+        if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
+        {
+            qDebug() << "Could not Read File";
+        }
+        QTextStream in(&file);
+
+        while(!in.atEnd())
+        {
+            purchaseDate = in.readLine();
+            id = in.readLine();
+            productName = in.readLine();
+            price = in.readLine();
+            purchaseQty = in.readLine();
+            status = in.readLine();
+
+            qDebug() << "Inserting purchase of " << productName << " for customer " << id;
+
+            db.exec("insert into salesReport (purchaseDate, id, productName, price, purchaseQty, status, day)"
+                    "values ('"+purchaseDate+"','"+id+"','"+productName+"','"+price+"','"+purchaseQty+"','"+status+"','"+"5"+"')");
+        }
+    }
+    {//Refreshing Day 6 Sales Report db
+        qDebug() << "Begining extraction for Day 6 sales report from .txt to .db";
+        QString purchaseDate;
+        QString id;
+        QString productName;
+        QString price;
+        QString purchaseQty;
+        QString status;
+
+        QFile file(":/textfiles/Resource Stuff/Database textfiles/day6.txt");
+        if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
+        {
+            qDebug() << "Could not Read File";
+        }
+        QTextStream in(&file);
+
+        while(!in.atEnd())
+        {
+            purchaseDate = in.readLine();
+            id = in.readLine();
+            productName = in.readLine();
+            price = in.readLine();
+            purchaseQty = in.readLine();
+            status = in.readLine();
+
+            qDebug() << "Inserting purchase of " << productName << " for customer " << id;
+
+            db.exec("insert into salesReport (purchaseDate, id, productName, price, purchaseQty, status, day)"
+                    "values ('"+purchaseDate+"','"+id+"','"+productName+"','"+price+"','"+purchaseQty+"','"+status+"','"+"6"+"')");
+        }
+    }
+    {//Refreshing Day 7 Sales Report db
+        qDebug() << "Begining extraction for Day 7 sales report from .txt to .db";
+        QString purchaseDate;
+        QString id;
+        QString productName;
+        QString price;
+        QString purchaseQty;
+        QString status;
+
+        QFile file(":/textfiles/Resource Stuff/Database textfiles/day7.txt");
+        if(!file.open(QIODevice::ReadOnly  | QIODevice::Text))
+        {
+            qDebug() << "Could not Read File";
+        }
+        QTextStream in(&file);
+
+        while(!in.atEnd())
+        {
+            purchaseDate = in.readLine();
+            id = in.readLine();
+            productName = in.readLine();
+            price = in.readLine();
+            purchaseQty = in.readLine();
+            status = in.readLine();
+
+            qDebug() << "Inserting purchase of " << productName << " for customer " << id;
+
+            db.exec("insert into salesReport (purchaseDate, id, productName, price, purchaseQty, status, day)"
+                    "values ('"+purchaseDate+"','"+id+"','"+productName+"','"+price+"','"+purchaseQty+"','"+status+"','"+"7"+"')");
+
+        }
     }
     conn.connClose();
 }
