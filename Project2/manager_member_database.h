99faabe4c612406f023c63888770decc24fff435
Lamson555 <54380548+Lamson555@users.noreply.github.com>
2019-11-26 19:30:54 -0800

    manager member data

--- Project2/manager_member_database.h
+++ Project2/manager_member_database.h
@@ -2,6 +2,7 @@
 #define MANAGER_MEMBER_DATABASE_H
 
 #include <QDialog>
+#include "mainwindow.h"
 
 namespace Ui {
 class manager_member_database;
@@ -12,8 +13,22 @@ class manager_member_database : public QDialog
     Q_OBJECT
 
 public:
+    MainWindow conn;
     explicit manager_member_database(QWidget *parent = nullptr);
     ~manager_member_database();
+
+private slots:
+    void on_returnButton_clicked();
+
+    void on_loadButton_clicked();
+
+    void on_tableView_doubleClicked(const QModelIndex &index);
+
+    void on_comboBox_currentIndexChanged(const QString &arg1);
+
+    void on_listView_clicked(const QModelIndex &index);
+
+    void on_tableView_activated(const QModelIndex &index);
 
 private:
     Ui::manager_member_database *ui;
