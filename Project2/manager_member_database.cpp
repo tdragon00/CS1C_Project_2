d33f890547c22385b71b0bb9fb52e2f0f2ffc03e
tdragon00 <47230462+tdragon00@users.noreply.github.com>
2019-12-08 13:50:44 -0800

    Update manager_member_database.cpp

--- Project2/manager_member_database.cpp
+++ Project2/manager_member_database.cpp
@@ -44,7 +44,7 @@ void manager_member_database::Load_Member_Data()
     conn.connOpen();
     QSqlQuery* qry = new QSqlQuery(conn.mydb);
 
-    qry->prepare("Select name,memberNum, status, expDate from customers");
+    qry->prepare("Select name,memberNum, status, expDate,totalPurchases from customers");
 
     qry->exec();
     modal->setQuery(*qry);
